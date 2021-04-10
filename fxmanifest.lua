ea246125da11a6ecfe3e10c6007879c538270b44
M_N•Script <67985905+maznaz@users.noreply.github.com>
2021-04-10 15:58:58 +0700

    update fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,25 @@
+fx_version 'cerulean'
+
+games { 'gta5' }
+
+author 'M_N•Script'
+
+description 'Job with props'
+
+version '1.0.0'
+
+
+client_scripts {
+    'client/main.lua'
+    'locales/id.lua'
+    'config.lua'
+}
+
+server_script {
+    'server/main.lua',
+    'locales/id.lua',
+    'config.lua'
+}
+
+dependencies {
+}
