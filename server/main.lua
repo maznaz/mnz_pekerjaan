908a3fbe2e7c414b684389df37d4edcb2779cfac
M_N•Script <67985905+maznaz@users.noreply.github.com>
2021-04-10 17:05:06 +0700

    update main.lua

--- server/main.lua
+++ server/main.lua
@@ -1,1 +1,26 @@
+ESX     = nil
+
+
+TriggerEvent("esx:getSharedObject", function(respone)
+    ESX = respone
+end)
+
+--Pick Aple
+RegisterServerEvent('mnz_PickApel:getItem')
+AddEventHandler('mnz_PickApel:getItem', function()
+    local luck = math.random (1, 3)
+    if luck == 1 then
+        local item = {
+            'apel'
+        }
+        local xPlayer = ESX.GetPlayerFromOd(source)
+        local randomitem = Item[math.random(#items)]
+        local quantity = math.random (items)
+        local itemfound = ESX.GetItemLabel(randomitem)
+        xPlayer.addInventoryItem()
+        TriggerClientEvent('NOTIF')
+    else
+        TriggerClientEvent('NOTIF NO')
+    end
+end)
 
