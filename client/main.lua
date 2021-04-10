0d3da2ab84086619e5e28cc23ca455f3118de072
M_N•Script <67985905+maznaz@users.noreply.github.com>
2021-04-10 16:31:06 +0700

    update main.lua

--- client/main.lua
+++ client/main.lua
@@ -1,1 +1,30 @@
+ESX         = nil
+CachePlant  = {}
+ClosetPlant = {
+    'prop_apel'
+}
+
+Citizen.CreatThread (function()
+    while true do
+        Citizen.Wait (12000)
+        CachePlant = {}
+    end
+end)
+
+Citizen.CreatThread (function()
+    while ESX == nil do
+        Citizen.Wait (5)
+        TriggerEvent("esx:getSharedObject", function (libary)
+            ESX = libary
+        end)
+    end
+    if ESX.IsPlayerLoaded() then
+        ESX.playerData = ESX.GetPlayerData()
+    end
+end)
+
+RigisterNetEvent("esx:playerLoaded")
+AddEventHandler("esx:playerLoaded", function(respone)
+    ESX.PlayerData = respone
+end)
 
