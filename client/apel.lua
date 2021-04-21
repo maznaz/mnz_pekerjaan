8c3f4513bfe0431efff5ce4f2c2816222c4c3ef0
M_N•Script <67985905+maznaz@users.noreply.github.com>
2021-04-21 14:32:26 +0700

    Add files via upload

---
+++ client/apel.lua
@@ -0,0 +1,59 @@
+ESX         = nil
+CachePlant  = {}
+ClosetPlant = {
+    'prop_tree_birch_05'
+}
+
+Citizen.CreatThread (function()
+    while true do
+        Citizen.Wait (12000)
+        CachePlant = {}
+    end
+end)
+
+Citizen.CreatThread (function()
+    while ESX == nil do
+        Citizen.Wait (5)
+        TriggerEvent("esx:getSharedObject", function (libary)
+            ESX = libary
+        end)
+    end
+    if ESX.IsPlayerLoaded() then
+        ESX.playerData = ESX.GetPlayerData()
+    end
+end)
+
+RigisterNetEvent("esx:playerLoaded")
+AddEventHandler("esx:playerLoaded", function(respone)
+    ESX.PlayerData = respone
+end)
+
+-- PETIK Apel
+Citizen.CreatThread (function()
+    while true do
+        local Sleep = 1000
+        local PlayerPed = PlayerPedId()
+        local PlayerCoords= GetEntityCoords(playerped)
+        for i = 1, #ClosetPlant do
+            local x= GetClosetObjectOfType(PlayerCoords, 2.0, GetHashKey(ClosetPlant[i]), false, false, false)
+            local entity = nil
+            if DoesEntityExsist(x) then
+                entity = x
+                plant = GetEntityCoords(entity)
+                sleep = 5
+                DrawText3D(plant.x, plant.y, plant.z, + 1.5 'Tekan [~g~E~s~] Untuk Memetik Apel ~b~apel~s~')
+                if IsControlJustReleased(0, 38) then
+                    if not CachePlant(entity)
+                    OpenPlantApel(entity)
+                else
+                    export['mythic_notify']:SendAlert('inform', 'text Client', 2500, {['background-color'] = '#ffffff', ['color'] = '#000000' })
+                end
+            end
+            break
+        else
+            sleep = 1000
+        end
+    end
+    Citizen.Wait (sleep)
+end
+end)
--- client/function.lua
+++ client/function.lua
@@ -1,3 +1,4 @@
+local CurrentAction
 -- draw 3D Text in Screen
 
 DrawText3D = function(x, y, z, text)
@@ -30,3 +31,87 @@ OpenPlantApel = function (entity)
     TtiggerServerEevent('mnz_PickAple:getItem')
     ClearPedTaskImmediately(PlayerPedId())
 end
+
+-- item JOB Sayuran
+
+OpenPlantSayur = function (entity)
+    TaskStartScenarioInPlace(PlayerPedId(), "task play anime", 0, true)
+    Citizen.Wait (10000)
+    cacheplant[entity] = true
+    TtiggerServerEevent('mnz_PickSayur:getItem')
+    ClearPedTaskImmediately(PlayerPedId())
+end
+-----------------------------------------------------------------------------------------------------------
+-- Display marker 
+Citizen.CreateThread(function()
+	while true do
+		Wait(0)
+		local coords = GetEntityCoords(GetPlayerPed(-1))
+
+		for k,v in pairs(Config.Zones) do
+			if(v.Type ~= -1 and GetDistanceBetweenCoords(coords, v.Pos.x, v.Pos.y, v.Pos.z, true) < Config.DrawDistance) then
+				DrawMarker(v.Type, v.Pos.x, v.Pos.y, v.Pos.z, 0.0, 0.0, 0.0, 0, 0.0, 0.0, v.Size.x, v.Size.y, v.Size.z, v.Color.r, v.Color.g, v.Color.b, 100, false, true, 2, false, false, false, false)
+			end
+		end
+	end
+end)
+
+
+--masuk dan keluar marker zone
+Citizen.CreateThread(function()
+	while true do
+		local coords      = GetEntityCoords(GetPlayerPed(-1))
+		local isInMarker  = false
+		local currentZone = nil
+
+		for k,v in pairs(Config.Zones) do
+			if(GetDistanceBetweenCoords(coords, v.Pos.x, v.Pos.y, v.Pos.z, true) < v.Size.x) then
+				isInMarker  = true
+				currentZone = k
+			end
+		end
+
+		if (isInMarker and not HasAlreadyEnteredMarker) or (isInMarker and LastZone ~= currentZone) then
+			HasAlreadyEnteredMarker = true
+			LastZone                = currentZone
+			TriggerEvent('mnz_pekerjaan:hasEnteredMarker', currentZone)
+		end
+
+		if not isInMarker and HasAlreadyEnteredMarker then
+			HasAlreadyEnteredMarker = false
+			TriggerEvent('mnz_pekerjaan:hasExitedMarker', LastZone)
+		end
+	end
+end)
+
+
+AddEventHandler('mnz_pekerjaan:hasEnteredMarker', function(zone)
+	if zone == 'TempatJual' --[[and PlayerData.job ~= nil and PlayerData.job.name == 'boulangerie']] then
+		CurrentAction     = 'tempat_jual'
+		CurrentActionMsg  = _U('press_sell')
+		CurrentActionData = {zone = zone}
+	end
+end)
+
+--seling
+Citizen.CreateThread(function()
+	while true do
+		Citizen.Wait(0)
+		if CurrentAction ~= nil then
+			SetTextComponentFormat('STRING')
+			AddTextComponentString(CurrentActionMsg)
+			DisplayHelpTextFromStringLabel(0, 0, 1, -1)
+
+			if IsControlPressed(0,  Keys['E']) and (GetGameTimer() - GUI.Time) > 300 then
+				if CurrentAction == 'tempat_jual' then
+					TriggerServerEvent('mnz_pekerjaan:startSell', CurrentActionData.zone)
+					Citizen.Wait(15000)
+				end
+
+				CurrentAction = nil
+				GUI.Time      = GetGameTimer()
+
+			end
+		end
+	end
+end)
